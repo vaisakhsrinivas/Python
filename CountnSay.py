b1453c78ac51744cedc249cc08a7d4989af3bd67
vaisakhsrinivas <vaisakhsrinivas.m@husky.neu.edu>
2020-12-07 14:14:12 -0500

    New commits

--- CountnSay.py
+++ CountnSay.py
@@ -9,22 +9,17 @@ def CountnSay(n):
 
         s += '$'
         l = len(s)
-
         cnt = 1
         tmp = ""
 
         for j in range(1 , l):
 
             if (s[j] != s[j - 1]):
-
-
                 tmp += str(cnt + 0)
                 tmp += s[j - 1]
                 cnt = 1
-
             else:
                 cnt += 1
-
         s = tmp
     return s
 
